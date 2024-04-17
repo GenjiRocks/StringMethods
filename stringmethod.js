a3927a0765574ac507ba6f0396e2e5da5e92e81b
Ashwin <ashwindeepak1997@gmail.com>
2024-04-17 11:03:54 +0530

    ver1

--- stringmethod.js
+++ stringmethod.js
@@ -1,9 +1,29 @@
 //  String Methods
 
-a = 'luminar'
+a = 'luMInar'
 
 // startsWith()
 console.log(a.startsWith('lum'));
 
 // endsWith()
 console.log(a.endsWith('nar'));
+
+//toUpperCase()
+console.log(a.toUpperCase()); 
+
+// toLowerCase()
+console.log(a.toLowerCase());
+
+// trim()
+str = '          asdfghjk'
+str1 = str.trim()
+console.log(str1);
+
+// substring()   (startindex,endindex) - endindex is not included in the new array
+
+str = 'luminar'
+
+str1 = str.substring(0,4)
+console.log(str1);
+str2 = str.substring(2)
+console.log(str2);
