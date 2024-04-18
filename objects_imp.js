876ad39f8ae33b4a8be0372159b52dc03d47a1f7
Ashwin <ashwindeepak1997@gmail.com>
2024-04-18 10:59:20 +0530

    ver1

---
+++ objects_imp.js
@@ -0,0 +1,15 @@
+// Objects example
+//[id, name, designation, location, salary, experience]
+// [1000,'Neel','developer','kochi',25000,3]
+
+employee={
+    id:1000,
+    Name:'Neel',
+    desig:'developer',
+    locations:'kochi',
+    salary:25000,
+    exp:3
+    
+}
+
+console.log(employee['desig']);
--- stringmethod.js
+++ stringmethod.js
@@ -27,3 +27,28 @@ str1 = str.substring(0,4)
 console.log(str1);
 str2 = str.substring(2)
 console.log(str2);
+
+// slice method
+
+str3 = 'luminar'
+str4 = str3.slice(1,4)
+console.log(str4);
+
+// minar
+str5 = str3.slice(-5)
+console.log(str5);
+
+// split method
+str = 'luminar technolab'
+str1 = str.split(' ')
+console.log(str1);
+
+// replace()
+
+str = 'i joined microsoft'
+str1 = str.replace('joined','quit')
+console.log(str1);
+
+str = 'hello world'
+str1 = str.replace(' ','')
+console.log(str1);
